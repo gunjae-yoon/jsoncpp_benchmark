0578744fcd00a079c14c3918c059d7bd19fb31ba
Gunjae Yoon <gjyoon@plaif.com>
2024-05-14 17:51:14 +0900

    Implement functional test method for checking DOM parsing result of RapidJSON

--- src/impl/rapidjson.cpp
+++ src/impl/rapidjson.cpp
@@ -1,4 +1,10 @@
 #include <impl/rapidjson.h>
+#include <rapidjson/document.h>
+#include <rapidjson/stringbuffer.h>
+#include <iostream>
+
+using rapidjson::Document;
+using rapidjson::Value;
 
 namespace jcbm {
 	RapidJson::RapidJson() {
@@ -8,6 +14,28 @@ namespace jcbm {
 	}
 
 	bool RapidJson::doesDomKeepOrder(const std::string& q, const std::list<std::string>& a) const {
+		// step 1. parse a JSON string into DOM
+		Document d;
+		d.Parse(q.c_str());
+		
+		// step 2. access sequentially
+		if (d.IsObject()) {
+			Value::ConstMemberIterator qIter = d.MemberBegin();
+			std::list<std::string>::const_iterator aIter = a.begin();
+			while (qIter != d.MemberEnd()) {
+				std::cout << "parsing result: " << qIter->name.GetString() << ", expected result: " << (*aIter) << std::endl;
+
+				// step 2.1. check result
+				if (qIter->name.GetString() != (*aIter)) {
+					return false;
+				}
+				
+				// step 2.2. do next
+				qIter++;
+				aIter++;
+			}
+		}
+
 		return true;
 	}
 
