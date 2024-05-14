d29649749c0064b0b72c439372cb5908ad88328e
Gunjae Yoon <gjyoon@plaif.com>
2024-05-14 15:50:34 +0900

    Add interface class for functional test

---
+++ src/base/functional.cpp
@@ -0,0 +1,12 @@
+#include <base/functional.h>
+
+namespace jcbm {
+	const Functional::QA Functional::question = { "value", {"alpha"}};
+	const Functional::QA Functional::answer = { "value", {"alpha"}};
+
+	Functional::Functional() {
+	}
+
+	Functional::~Functional() {
+	}
+}
