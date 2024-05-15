e0043c09c63d2bd1f07c6fdada4773780c5cd36e
Gunjae Yoon <gjyoon@plaif.com>
2024-05-16 08:36:45 +0900

    Implement functional test method for checking SAX parsing result of RapicJSON

--- src/impl/rapidjson.cpp
+++ src/impl/rapidjson.cpp
@@ -40,6 +40,36 @@ namespace jcbm {
 	}
 
 	bool RapidJson::doesSaxKeepOrder(const std::list<std::string>& q, const std::list<std::string>& a) const {
+		// step 1. create an empty JSON object
+		Document d;
+		d.SetObject();
+		Document::AllocatorType& allocator = d.GetAllocator();
+		
+		// step 2. add members
+		for (const std::string& elem: q) {
+			Value key;
+			key.SetString(elem.c_str(), allocator);
+			d.AddMember(key, "", allocator);
+		}
+
+		// step 3. access sequentially
+		if (d.IsObject()) {
+			Value::ConstMemberIterator qIter = d.MemberBegin();
+			std::list<std::string>::const_iterator aIter = a.begin();
+			while (qIter != d.MemberEnd()) {
+				std::cout << "parsing result: " << qIter->name.GetString() << ", expected result: " << (*aIter) << std::endl;
+
+				// step 2.1. check result
+				if (qIter->name.GetString() != (*aIter)) {
+					return false;
+				}
+				
+				// step 2.2. do next
+				qIter++;
+				aIter++;
+			}
+		}
+
 		return true;
 	}
 }
