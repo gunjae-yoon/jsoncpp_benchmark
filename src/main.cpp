f769dc5ffcb95d08a9e19210d4c09401693fee5d
Gunjae Yoon <gjyoon@plaif.com>
2024-05-16 08:36:45 +0900

    Implement functional test method for checking SAX parsing result of RapicJSON

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,28 @@
 #include <version.h>
 #include <iostream>
 
+#include <impl/rapidjson.h>
+
 using namespace jcbm;
 
 int main(int argc, char** argv) {
 	std::cout << "version: " << Version::full << std::endl;
+	
+	RapidJson rapid;
+
+	std::cout << std::endl;
+	if (rapid.doesDomKeepOrder()) {
+		std::cout << "passed" << std::endl;
+	} else {
+		std::cout << "failed" << std::endl;
+	}
+
+	std::cout << std::endl;
+	if (rapid.doesSaxKeepOrder()) {
+		std::cout << "passed" << std::endl;
+	} else {
+		std::cout << "failed" << std::endl;
+	}
+
 	return 0;
 }
