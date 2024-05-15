8a447b501a61f53726071c0563dfb3cc2dc4741b
Gunjae Yoon <gjyoon@plaif.com>
2024-05-16 08:38:04 +0900

    Add conditions about failures in functional test methods

--- src/impl/rapidjson.cpp
+++ src/impl/rapidjson.cpp
@@ -34,6 +34,8 @@ namespace jcbm {
 				qIter++;
 				aIter++;
 			}
+		} else {
+			return false;
 		}
 
 		return true;
@@ -68,6 +70,8 @@ namespace jcbm {
 				qIter++;
 				aIter++;
 			}
+		} else {
+			return false;
 		}
 
 		return true;
