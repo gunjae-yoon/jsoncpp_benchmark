95e085a81b1392a1638cbf21f4363f2d10810e4e
Gunjae Yoon <gjyoon@plaif.com>
2024-05-16 09:17:03 +0900

    Fix local variable name to read easier

--- src/impl/rapidjson.cpp
+++ src/impl/rapidjson.cpp
@@ -15,14 +15,14 @@ namespace jcbm {
 
 	bool RapidJson::doesDomKeepOrder(const std::string& q, const std::list<std::string>& a) const {
 		// step 1. parse a JSON string into DOM
-		Document d;
-		d.Parse(q.c_str());
+		Document document;
+		document.Parse(q.c_str());
 		
 		// step 2. access sequentially
-		if (d.IsObject()) {
-			Value::ConstMemberIterator qIter = d.MemberBegin();
+		if (document.IsObject()) {
+			Value::ConstMemberIterator qIter = document.MemberBegin();
 			std::list<std::string>::const_iterator aIter = a.begin();
-			while (qIter != d.MemberEnd()) {
+			while (qIter != document.MemberEnd()) {
 				std::cout << "parsing result: " << qIter->name.GetString() << ", expected result: " << (*aIter) << std::endl;
 
 				// step 2.1. check result
@@ -43,22 +43,22 @@ namespace jcbm {
 
 	bool RapidJson::doesSaxKeepOrder(const std::list<std::string>& q, const std::list<std::string>& a) const {
 		// step 1. create an empty JSON object
-		Document d;
-		d.SetObject();
-		Document::AllocatorType& allocator = d.GetAllocator();
+		Document document;
+		document.SetObject();
+		Document::AllocatorType& allocator = document.GetAllocator();
 		
 		// step 2. add members
 		for (const std::string& elem: q) {
 			Value key;
 			key.SetString(elem.c_str(), allocator);
-			d.AddMember(key, "", allocator);
+			document.AddMember(key, "", allocator);
 		}
 
 		// step 3. access sequentially
-		if (d.IsObject()) {
-			Value::ConstMemberIterator qIter = d.MemberBegin();
+		if (document.IsObject()) {
+			Value::ConstMemberIterator qIter = document.MemberBegin();
 			std::list<std::string>::const_iterator aIter = a.begin();
-			while (qIter != d.MemberEnd()) {
+			while (qIter != document.MemberEnd()) {
 				std::cout << "parsing result: " << qIter->name.GetString() << ", expected result: " << (*aIter) << std::endl;
 
 				// step 3.1. check result
