84368c679683a805f22789b84d814117b47b63ea
Gunjae Yoon <gjyoon@plaif.com>
2024-05-16 14:32:19 +0900

    Add members and interfaces for measuring parse performances

--- src/base/performance.cpp
+++ src/base/performance.cpp
@@ -1,6 +1,9 @@
 #include <base/performance.h>
 
 namespace jcbm {
+	std::filesystem::path Performance::FLAT_TEXT_FILE_PATH = "";
+	std::filesystem::path Performance::NESTED_TEXT_FILE_PATH = "";
+
 	Performance::Performance() {
 	}
 
--- src/base/performance.h
+++ src/base/performance.h
@@ -1,11 +1,21 @@
 #ifndef __jsoncpp_benchmark_base_performance_h__
 #define __jsoncpp_benchmark_base_performance_h__
 
+#include <chrono>
+#include <filesystem>
+
 namespace jcbm {
 	class Performance {
 	public:
+		static std::filesystem::path FLAT_TEXT_FILE_PATH;
+		static std::filesystem::path NESTED_TEXT_FILE_PATH;
+
+	public:
 		Performance();
 		virtual ~Performance();
+		
+		virtual std::chrono::nanoseconds parseFlatText() const = 0;
+		virtual std::chrono::nanoseconds parseNestedText() const = 0;
 	};
 }
 
