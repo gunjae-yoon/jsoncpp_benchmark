11d7983a9fd8d0c212261554267ec95460c87b08
Gunjae Yoon <gjyoon@plaif.com>
2024-05-17 15:13:58 +0900

    Implement functional test method for checking DOM parsing result of taoJSON

--- src/impl/taojson.cpp
+++ src/impl/taojson.cpp
@@ -1,9 +1,11 @@
 #include <impl/taojson.h>
+#include <tao/json.hpp>
 #include <iostream>
 #include <fstream>
 #include <sstream>
 #include <vector>
 
+using namespace tao;
 using namespace std::chrono_literals;
 
 namespace jcbm {
@@ -14,6 +16,29 @@ namespace jcbm {
 	}
 
 	bool TaoJson::doesDomKeepOrder(const std::string& q, const std::list<std::string>& a) const {
+		// step 1. parse a JSON string into DOM
+		json::value document = tao::json::from_string(q);
+
+		// step 2. access sequentially
+		if (document.is_object()) {
+			json::value::object_t::const_iterator qIter = document.get_object().begin();
+			std::list<std::string>::const_iterator aIter = a.begin();
+			while (qIter != document.get_object().end()) {
+				std::cout << "parsing result: " << qIter->first << ", expected result: " << (*aIter) << std::endl;
+				
+				// step 2.1. check result
+				if (qIter->first != (*aIter)) {
+					return false;
+				}
+		
+				// step 2.2. do next
+				qIter++;
+				aIter++;
+			}
+		} else {
+			return false;
+		}
+
 		return true;
 	}
 
