d710717851b49bb866a58c07b4e0e8bf7307d10b
Gunjae Yoon <gjyoon@plaif.com>
2024-05-17 15:17:20 +0900

    Implement functional test method for checking SAX parsing result of taoJSON

--- src/impl/taojson.cpp
+++ src/impl/taojson.cpp
@@ -43,6 +43,34 @@ namespace jcbm {
 	}
 
 	bool TaoJson::doesSaxKeepOrder(const std::list<std::string>& q, const std::list<std::string>& a) const {
+		// step 1. create an empty JSON object
+		json::value document = json::empty_object;
+		
+		// step 2. add members
+		for (const std::string& elem: q) {
+			document[elem] = "";
+		}
+
+		// step 3. access sequentially
+		if (document.is_object()) {
+			json::value::object_t::const_iterator qIter = document.get_object().begin();
+			std::list<std::string>::const_iterator aIter = a.begin();
+			while (qIter != document.get_object().end()) {
+				std::cout << "parsing result: " << qIter->first << ", expected result: " << (*aIter) << std::endl;
+				
+				// step 3.1. check result
+				if (qIter->first != (*aIter)) {
+					return false;
+				}
+		
+				// step 3.2. do next
+				qIter++;
+				aIter++;
+			}
+		} else {
+			return false;
+		}
+
 		return true;
 	}
 
