ea959f74e8a88827bd7bac933554a10bcdda085b
Gunjae Yoon <gjyoon@plaif.com>
2024-05-17 15:20:29 +0900

    Implement parsing time measuring methods of taoJSON

--- src/impl/taojson.cpp
+++ src/impl/taojson.cpp
@@ -75,10 +75,68 @@ namespace jcbm {
 	}
 
 	std::chrono::nanoseconds TaoJson::parseFlatText() const {
-		return 0ns;
+		// step 1. read stream from file
+		std::ifstream file(Performance::FLAT_TEXT_FILE_PATH);
+		if (file.is_open() == false) {
+			return 0ns;
+		}
+
+		std::stringstream stream;
+		stream << file.rdbuf();
+		std::string text = stream.str();
+
+		// step 2. parse a JSON string into DOM
+		json::value document = json::empty_object;
+		std::vector<std::chrono::nanoseconds> results;
+		
+		for (size_t cnt = 0; cnt < Performance::REPEAT_COUNT; cnt++) {
+			std::chrono::time_point begin = std::chrono::system_clock::now();
+			document = json::from_string(text.c_str());
+			std::chrono::time_point end = std::chrono::system_clock::now();
+			results.push_back(end - begin);
+			document = json::empty_object;
+			//printf("[%2lu] result: %9lu\n", cnt, results[cnt].count());
+		}
+		
+		uint64_t total = 0;
+		for (std::chrono::nanoseconds& result : results) {
+			total += result.count();
+		}
+		//printf("average: %9lu\n", (total / Performance::REPEAT_COUNT));
+
+		return std::chrono::nanoseconds(total / Performance::REPEAT_COUNT);
 	}
 
 	std::chrono::nanoseconds TaoJson::parseNestedText() const {
-		return 0ns;
+		// step 1. read stream from file
+		std::ifstream file(Performance::NESTED_TEXT_FILE_PATH);
+		if (file.is_open() == false) {
+			return 0ns;
+		}
+
+		std::stringstream stream;
+		stream << file.rdbuf();
+		std::string text = stream.str();
+
+		// step 2. parse a JSON string into DOM
+		json::value document = json::empty_object;
+		std::vector<std::chrono::nanoseconds> results;
+		
+		for (size_t cnt = 0; cnt < Performance::REPEAT_COUNT; cnt++) {
+			std::chrono::time_point begin = std::chrono::system_clock::now();
+			document = json::from_string(text.c_str());
+			std::chrono::time_point end = std::chrono::system_clock::now();
+			results.push_back(end - begin);
+			document = json::empty_object;
+			//printf("[%2lu] result: %9lu\n", cnt, results[cnt].count());
+		}
+		
+		uint64_t total = 0;
+		for (std::chrono::nanoseconds& result : results) {
+			total += result.count();
+		}
+		//printf("average: %9lu\n", (total / Performance::REPEAT_COUNT));
+
+		return std::chrono::nanoseconds(total / Performance::REPEAT_COUNT);
 	}
 }
